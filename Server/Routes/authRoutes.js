aea2b861383532d6a05d79d2fe745f36f6a0dfac
Muhammad-Zeeshan-C <m.zeeeshankhalid12@gmail.com>
2026-07-29 12:43:04 +0500

    FEAT: Add authentication middleware to user route and update package scripts

--- Server/Routes/authRoutes.js
+++ Server/Routes/authRoutes.js
@@ -2,12 +2,13 @@ import express from "express";
 import { registerUser } from "../Controllers/authController.js";
 import { loginUser } from "../Controllers/authController.js";
 import { getCurrentUser } from "../Controllers/authController.js";
+import isAuthenticated from '../Controllers/authentication.js'
 
 
 const authRouter = express.Router();
 
 authRouter.post("/register",registerUser)
 authRouter.post("/login",loginUser)
-authRouter.get("/user",getCurrentUser)
+authRouter.get("/user",isAuthenticated,getCurrentUser)
 
 export default authRouter;
